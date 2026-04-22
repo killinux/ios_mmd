f36bb8f0b7329ba145db00c81e93f17dfc2a8d3b
killinux <killinux@163.com>
2026-04-22 14:57:31 +0800

    Initial Commit

---
+++ ios_mmd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_mmd
+//
+//  Created by ByteDance on 2026/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_mmd/ios_mmdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_mmdApp.swift
+//  ios_mmd
+//
+//  Created by ByteDance on 2026/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_mmdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
